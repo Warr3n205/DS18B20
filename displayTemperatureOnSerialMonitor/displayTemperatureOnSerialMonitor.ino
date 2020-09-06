feb8752b12f2c9d71beb7e2cff1f892fe7fd1fc9
Warr3n205 <68779758+Warr3n205@users.noreply.github.com>
2020-09-06 16:34:17 +0800

    Update displayTemperatureOnSerialMonitor.ino
    Wrote bade code

--- displayTemperatureOnSerialMonitor/displayTemperatureOnSerialMonitor.ino
+++ displayTemperatureOnSerialMonitor/displayTemperatureOnSerialMonitor.ino
@@ -1,1 +1,22 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+#define ONE_WIRE_BUS 2
+
+OneWire oneWire(ONE_WIRE_BUS);  
+
+DallasTemperature sensors(&oneWire);
+
+void setup(void) {
+  sensors.begin();  
+  Serial.begin(9600);
+}
+void loop(void) { 
+  sensors.requestTemperatures(); 
+
+  Serial.print("Temperature: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.print("°C");
+  
+  delay(1000);
+}
